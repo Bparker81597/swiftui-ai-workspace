80e70e72edd42d537e3274b3d5065d3a0f72c618
Nathan Baker <nathan.baker@atomicobject.com>
2026-03-30 10:24:07 -0400

    Initial Commit

---
+++ SwiftUIAIWorkspace/SwiftUIAIWorkspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAIWorkspaceApp.swift
+//  SwiftUIAIWorkspace
+//
+//  Created by myMac on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAIWorkspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIAIWorkspaceTests/SwiftUIAIWorkspaceTests.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIAIWorkspaceTests.swift
+//  SwiftUIAIWorkspaceTests
+//
+//  Created by myMac on 3/30/26.
+//
+
+import Testing
+@testable import SwiftUIAIWorkspace
+
+struct SwiftUIAIWorkspaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
